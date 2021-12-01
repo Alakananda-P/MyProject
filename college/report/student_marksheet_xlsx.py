f47e8f50e35a5f270a54b36af4c52e087fa7abef
Alakananda-P <alakanandapktr@gmail.com>
2021-12-01 22:54:24 +0530

    [ADD]college

--- college/report/student_marksheet_xlsx.py
+++ college/report/student_marksheet_xlsx.py
@@ -8,44 +8,101 @@ class StudentMarksheetXlsx(models.AbstractModel):
 
     def generate_xlsx_report(self, workbook, data, students):
         sheet = workbook.add_worksheet('Student MarkSheet')
-        head = workbook.add_format({'bold': True, 'align': 'center', 'font_size': '14px'})
-        sub_head = workbook.add_format({'align': 'center', 'bold': True})
+        head = workbook.add_format({'bold': True,
+                                    'align': 'center',
+                                    'font_size': '14px'})
+        sub_head = workbook.add_format({'align': 'center',
+                                        'bold': True})
         bold = workbook.add_format({'bold': True})
         form_data = data['form']
         student = data['student']
-        # if form_data
-        sheet.set_column('A:A', 30)
-        sheet.set_column('B:B', 12)
-        sheet.set_column('C:C', 20)
-        sheet.set_column('D:D', 18)
-        student_marklist = form_data['student_id'][1] + ': ' + 'Mark List'
-        sheet.merge_range('A1:D1', student_marklist, head)
-        course_academic_year = form_data['student_course_id'] + '-' + form_data['student_academic_year']
-        sheet.merge_range('A2:D2', course_academic_year, sub_head)
-        if form_data['exam_type_id']:
-            exam = 'Exam' + ': ' + form_data['exam_type_id']
-            sheet.merge_range('A3:C3', exam, bold)
-        else:
-            exam = 'Exam' + ': ' + ' '
-            sheet.merge_range('A3:C3', exam, bold)
-        if student[0]['pass_fail'] == True:
-            result = 'Result' + ': ' + 'Pass'
-            sheet.merge_range('A4:C4', result, bold)
-        else:
-            result = 'Exam' + ': ' + 'Failed'
-            sheet.merge_range('A4:C4', result, bold)
-        row = 4
-        col = 0
-        sheet.write(row, col, 'Subject', bold)
-        sheet.write(row, col+1, 'Mark', bold)
-        sheet.write(row, col+2, 'Pass Mark', bold)
-        sheet.write(row, col+3, 'Pass/Fail', bold)
-        for line in student:
-            row += 1
-            sheet.write(row, col, line['subject'])
-            sheet.write(row, col+1, line['mark'])
-            sheet.write(row, col+2, line['passmark'])
-            if line['subject_pass_fail'] == True:
-                sheet.write(row, col+3, 'Pass')
+        count = data['count']
+        subject_mark = data['subject_mark']
+        if form_data['marklist'] == 'student':
+            sheet.set_column('A:A', 30)
+            sheet.set_column('B:B', 12)
+            sheet.set_column('C:C', 20)
+            sheet.set_column('D:D', 18)
+            student_marklist = form_data['student_id'][1] + ': ' + 'Mark List'
+            sheet.merge_range('A1:D1', student_marklist, head)
+            course_academic_year = form_data['student_course_id'
+                                   ] + '-' + form_data['student_academic_year']
+            sheet.merge_range('A2:D2', course_academic_year, sub_head)
+            if form_data['exam_type_id']:
+                exam = 'Exam' + ': ' + form_data['exam_type_id']
+                sheet.merge_range('A3:C3', exam, bold)
             else:
-                sheet.write(row, col+3, 'Fail')
+                exam = 'Exam' + ': ' + ' '
+                sheet.merge_range('A3:C3', exam, bold)
+            if student[0]['pass_fail'] == True:
+                result = 'Result' + ': ' + 'Pass'
+                sheet.merge_range('A4:C4', result, bold)
+            else:
+                result = 'Result' + ': ' + 'Failed'
+                sheet.merge_range('A4:C4', result, bold)
+            row = 4
+            col = 0
+            sheet.write(row, col, 'Subject', bold)
+            sheet.write(row, col+1, 'Mark', bold)
+            sheet.write(row, col+2, 'Pass Mark', bold)
+            sheet.write(row, col+3, 'Pass/Fail', bold)
+            for line in student:
+                row += 1
+                sheet.write(row, col, line['subject'])
+                sheet.write(row, col+1, line['mark'])
+                sheet.write(row, col+2, line['passmark'])
+                if line['subject_pass_fail'] == True:
+                    sheet.write(row, col+3, 'Pass')
+                else:
+                    sheet.write(row, col+3, 'Fail')
+        if form_data['marklist'] == 'class':
+            sheet.set_column('A:A', 30)
+            sheet.set_column('B:B', 20)
+            sheet.set_column('C:C', 20)
+            sheet.set_column('D:D', 20)
+            sheet.set_column('E:E', 20)
+            sheet.set_column('F:F', 20)
+            sheet.set_column('G:G', 20)
+            sheet.set_column('H:H', 15)
+            sheet.set_column('I:I', 15)
+            sheet.set_column('J:J', 18)
+            student_marklist = form_data['class_id'][1] + ': ' + 'Mark List'
+            sheet.merge_range('A1:J1', student_marklist, head)
+            course_academic_year = form_data['class_course_id'][1] + '-' + form_data['class_academic_year']
+            sheet.merge_range('A2:J2', course_academic_year, sub_head)
+            if form_data['exam_type_id']:
+                exam = 'Exam' + ': ' + form_data['exam_type_id']
+                sheet.merge_range('A3:C3', exam, bold)
+            else:
+                exam = 'Exam' + ': ' + ' '
+                sheet.merge_range('A3:C3', exam, bold)
+            total_student = 'Total' + ': ' + str(count[0][0]['total_student'])
+            sheet.merge_range('A4:C4', total_student, bold)
+            total_pass = 'Pass' + ': ' + str(count[0][0]['total_pass'])
+            sheet.merge_range('A5:C5', total_pass, bold)
+            total_fail = 'Fail' + ': ' + str(count[0][0]['total_fail'])
+            sheet.merge_range('A6:C6', total_fail, bold)
+            total_ratio = 'Ratio' + ': ' + str(count[0][0]['ratio'])
+            sheet.merge_range('A7:C7', total_ratio, bold)
+            row = 7
+            col = 0
+            sheet.write(row, col, 'Student Name', bold)
+            for subject in subject_mark[0]:
+                col += 1
+                sheet.write(row, col, subject['subject'], bold)
+            sheet.write(row, col + 1, 'Obtained Mark', bold)
+            sheet.write(row, col + 2, 'Total Mark', bold)
+            sheet.write(row, col + 3, 'Pass/Failed', bold)
+            for line in subject_mark:
+                row += 1
+                col = 0
+                sheet.write(row, col, line[0]['student'])
+                for mark in line:
+                    col += 1
+                    sheet.write(row, col, mark['mark'])
+                sheet.write(row, col+1, line[0]['obtained_mark'])
+                sheet.write(row, col+2, line[0]['total_mark'])
+                if line[0]['pass_fail'] == True:
+                    sheet.write(row, col+3, 'Pass')
+                else:
+                    sheet.write(row, col+3, 'Fail')
--- college/wizard/student_marksheet.py
+++ college/wizard/student_marksheet.py
@@ -216,6 +216,8 @@ class StudentMarksheetWizard(models.TransientModel):
         #     ratio = str(int(pass_count / div)) + ':' + str(int(
         #         fail_count / div))
     def action_print_excel(self):
+        subject_mark = []
+        count_values = []
         if self.marklist == 'student' and self.semester_id and self.exam_id:
             args = {
                 'student_id': self.student_id.name,
@@ -267,11 +269,128 @@ class StudentMarksheetWizard(models.TransientModel):
                                 LEFT OUTER JOIN marksheet_marks_lines m ON(
                                     cm.id = m.mark_id)
                                 WHERE (cm.name = %(student_id)s)""", args)
+        elif self.marklist == 'class' and self.semester_id and self.exam_id:
+            args = {
+                'class_id': self.class_id.name,
+                'semester_id': self.semester_id.name,
+                'exam_id': self.exam_id.name
+            }
+            self.env.cr.execute("""SELECT 
+                                    cm.name as student,
+                                    m.subject as subject,
+                                    m.mark as mark,                                 
+                                    cm.total_mark as obtained_mark,
+                                    cm.total_max_mark as total_mark,
+                                    cm.pass_fail as pass_fail
+                                FROM college_marksheet cm
+                                LEFT OUTER JOIN marksheet_marks_lines m 
+                                ON(cm.id = m.mark_id) 
+                                WHERE (cm.classes = %(class_id)s 
+                                AND cm.semester = %(semester_id)s 
+                                AND cm.exam = %(exam_id)s )""", args)
 
-        student = self.env.cr.dictfetchall()
+            student = self.env.cr.dictfetchall()
+
+            self.env.cr.execute("""SELECT
+                                    COUNT(cm.id) as total_student,
+                                    COUNT(CASE WHEN cm.pass_fail IS NULL or false THEN 1 END) as total_fail,
+                                    COUNT(cm.pass_fail = true) as total_pass,
+                                    ROUND((COUNT(NULLIF(cm.pass_fail,false))/COUNT(cm.id)::float)*100) as ratio
+                                    FROM college_marksheet cm
+                                    WHERE (cm.classes = %(class_id)s
+                                    AND cm.semester = %(semester_id)s
+                                    AND cm.exam = %(exam_id)s )""", args)
+            count = self.env.cr.dictfetchall()
+            count_values.append(count)
+
+            def key_func(k):
+                return k['student']
+            info = sorted(student, key=key_func)
+            for key, value in groupby(info, key_func):
+                # result.append(key)
+                subject_mark.append(list(value))
+
+        elif self.marklist == 'class' and self.semester_id:
+            args = {
+                'class_id': self.class_id.name,
+                'semester_id': self.semester_id.name
+            }
+            self.env.cr.execute("""SELECT 
+                                    cm.name as student,
+                                    m.subject as subject,
+                                    m.mark as mark,                                 
+                                    cm.total_mark as obtained_mark,
+                                    cm.total_max_mark as total_mark,
+                                    cm.pass_fail as pass_fail
+                                FROM college_marksheet cm
+                                LEFT OUTER JOIN marksheet_marks_lines m 
+                                ON(cm.id = m.mark_id) 
+                                WHERE (cm.classes = %(class_id)s 
+                                AND cm.semester = %(semester_id)s )""", args)
+
+            student = self.env.cr.dictfetchall()
+
+            self.env.cr.execute("""SELECT
+                                    COUNT(cm.id) as total_student,
+                                    COUNT(CASE WHEN cm.pass_fail IS NULL or false THEN 1 END) as total_fail,
+                                    COUNT(cm.pass_fail = true) as total_pass,
+                                    ROUND((COUNT(NULLIF(cm.pass_fail,false))/COUNT(cm.id)::float)*100) as ratio
+                                    FROM college_marksheet cm
+                                    WHERE (cm.classes = %(class_id)s
+                                    AND cm.semester = %(semester_id)s)""", args)
+            count = self.env.cr.dictfetchall()
+            count_values.append(count)
+
+            def key_func(k):
+                return k['student']
+
+            info = sorted(student, key=key_func)
+            for key, value in groupby(info, key_func):
+                # result.append(key)
+                subject_mark.append(list(value))
+
+        elif self.marklist == 'class':
+            args = {
+                'class_id': self.class_id.name
+            }
+            self.env.cr.execute("""SELECT 
+                                    cm.name as student,
+                                    m.subject as subject,
+                                    m.mark as mark,                                 
+                                    cm.total_mark as obtained_mark,
+                                    cm.total_max_mark as total_mark,
+                                    cm.pass_fail as pass_fail
+                                FROM college_marksheet cm
+                                LEFT OUTER JOIN marksheet_marks_lines m 
+                                ON(cm.id = m.mark_id) 
+                                WHERE (cm.classes = %(class_id)s )""", args)
+
+            student = self.env.cr.dictfetchall()
+
+            self.env.cr.execute("""SELECT
+                                    COUNT(cm.id) as total_student,
+                                    COUNT(CASE WHEN cm.pass_fail IS NULL or false THEN 1 END) as total_fail,
+                                    COUNT(cm.pass_fail = true) as total_pass,
+                                    ROUND((COUNT(NULLIF(cm.pass_fail,false))/COUNT(cm.id)::float)*100) as ratio
+                                    FROM college_marksheet cm
+                                    WHERE (cm.classes = %(class_id)s)""", args)
+            count = self.env.cr.dictfetchall()
+            count_values.append(count)
+
+            def key_func(k):
+                return k['student']
+
+            info = sorted(student, key=key_func)
+            for key, value in groupby(info, key_func):
+                # result.append(key)
+                subject_mark.append(list(value))
+
+        result = self.env.cr.dictfetchall()
         data = {
             'form': self.read()[0],
-            'student': student
+            'count': count_values,
+            'subject_mark': subject_mark,
+            'student': result
         }
         return self.env.ref('college.action_report_student_marksheet_xlsx'
                             ).report_action(self, data=data)
